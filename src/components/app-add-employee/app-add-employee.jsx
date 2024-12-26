504ec6c25f0c2e333b7b69fabe67e6381caa3e8b
unknown <you@example.com>
2024-12-26 18:12:41 +0400

    tried to do onsubmit function

--- src/components/app-add-employee/app-add-employee.jsx
+++ src/components/app-add-employee/app-add-employee.jsx
@@ -2,44 +2,43 @@ import { Component } from "react"
 import "./app-add-employee.css"
 
 export default class AddEmployee extends Component{
+  
   constructor(props){
     super(props);
+    const {data} = this.state.data
     this.state={
       name: "",
-      salary:0
+      salary:0 
     }
   }
  
   onInputValueChange =(e)=>{
+    
     this.setState({
       [e.target.name]: e.target.value
     })
   }
-//  onSubmit = this.onSubmit.bind(this);
-//   onSubmit =(e)=>{
-//     this.setState([
-//       ...data
-//     ],
-//     {
-//       id:Math.random(),
-//       name:"",
-//       salary:"",
-//       increase:false
-//     }
+ onSubmit = this.onSubmit.bind(this);
+
+  onSubmit =(e)=>{
+    this.setState([
+      ({...data})
+    ],
+    {
+      id:Math.random(),
+      name:"",
+      salary:"",
+      increase:false
+    }
       
       
-//     )
-//   }
+    )
+  }
   
     render(){
       return(
         <form
-        //  onSubmit={(e)=()=>{
-        //   e.preventDefault(),
-        //   onSubmit(this.state.name)
-
-        // }
-        // }
+        onSubmit={this.onSubmit}
         className="add-employee">
         <h3>  Add new Employee</h3>
   <input
